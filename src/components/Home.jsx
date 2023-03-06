e2308ff00d8da033e2450310a3a3e27ccd651fda
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2023-03-06 17:30:29 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,5 @@
-import React,{useEffect, useState} from 'react'
-import { initialState } from './../redux/reduce'
+import React from 'react'
 export default function Home() {
-  const [questions, setQuestions] = useState(initialState)
-  /* Save data In Local Srorage */
-  useEffect(() => {
-    localStorage.setItem("Questions", JSON.stringify(questions))
-},)
-/* Get data from Local Storage */
-useEffect(() => {
-   const Questions = localStorage.getItem("Questions")
-  setQuestions(JSON.parse(Questions))
-},[])
   return (
     <div className='flex justify-center w-full '>
       <h1 className='text-3xl text-cyan-900 p-5'>Home</h1>
