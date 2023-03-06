1186d6715060a8090cec8d1de9b24dbe5531ebc7
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2023-03-06 17:30:17 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import React from 'react'
 import { Routes, Route } from "react-router-dom"
 import Home from "./components/Home"
 import Admin from "./components/Admin"
 import Quizz from "./components/Quizz"
 import Navbar from "./Navbar"
+
 function App() {
 
     return (
